512d221b3bcdf9b6066ba9b6941e720f67857841
aryaver <arya.verma.923@gmail.com>
2023-10-13 19:15:46 +0530

    final test code

--- senti_ana_code.py
+++ senti_ana_code.py
@@ -6,15 +6,35 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 import nltk
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
 
 nltk.download("stopwords")
 
 data=pd.read_csv("https://confrecordings.ams3.digitaloceanspaces.com/amazon_alexa.tsv",delimiter='\t')
  
+app = dash.Dash(external_stylesheets=[dbc.themes.SKETCHY])
+server = app.server
 
+def check_response(arg):
+    if arg == "yes":
+        return "positive"
+    else:
+        return "negative"
 
-#print(data.head())
+app.layout = html.Div(children = [
+    html.H1("Sentiment Analysis", className="text-center fw-bold text-decoration-underline", style={'color': 'black', 'padding':'30px'}),
+    html.Br(),
+    html.Div(dbc.Row(dbc.Col(dbc.Input(id='sender-text-input', type='text', 
+                                       placeholder='Enter your text...', style={'width': '50vw'}),width={'size': 6, 'offset': 3}))),
+    html.Br(),
+    html.Div(dbc.Button('Check Analysis result', id = 'check-analysis', n_clicks = 0, color="warning", className = 'text-center')),
+    html.Div(html.Div(id = 'analysis-output',className='justify-content-center'),className='justify-content-center')
+], className="text-center")
+# #print(data.head())
 
 tokenizer=RegexpTokenizer(r'\w+')
 
@@ -115,15 +135,33 @@ def classify(text_box_1):
    return 'negative'
 
 
+@app.callback(
+   Output('analysis-output', 'children'),
+   Input('sender-text-input', 'value'),
+   Input('check-analysis', 'n_clicks'),
+)
+def sentiment_analysis(input_text, n_clicks):
+   if n_clicks > 0 and input_text is not None:
+    jumbotron = dbc.Row([dbc.Col(html.Div(
+                                        [   html.H2(f"{classify(input_text)}", className="display-3"),
+                                            html.Hr(className="my-2"),
+                                            html.P(f"{input_text} shows {classify(input_text)} sentiment!"
+                                        )],
+                                        className="h-100 p-5 text-black rounded-3 justify-content-center"))])
+         
+    return jumbotron
+
    
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8040)
 
-print(classify('This alexa Echo is awesome . I recommend you also to purchase this product.'))
+# print(classify('This alexa Echo is awesome . I recommend you also to purchase this product.'))
 
-print(classify('I like this product very much.'))
+# print(classify('I like this product very much.'))
 
-print(classify('I do not like any product of alexa.'))
+# print(classify('I do not like any product of alexa.'))
 
-print(classify('It’s sound is loud , clear , free from echo and pleasant.'))
+# print(classify('It’s sound is loud , clear , free from echo and pleasant.'))
 
-print(classify('I had complained about this product numerous time but get no reply.'))
-
+# print(classify('I had complained about this product numerous time but get no reply.'))
